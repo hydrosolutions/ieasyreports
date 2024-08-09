b1da7c6cd9c63a3727069071c235556a156791fc
Davor Škalec <davor.skalec@encode.hr>
2024-08-09 13:34:06 +0200

    Bump version: 1.0.1 → 1.1.0

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hydrosolutions/ieasyreports',
-    version='1.0.1',
+    version='1.1.0',
     zip_safe=False,
 )
